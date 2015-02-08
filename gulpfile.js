44df23ce2c98a720f548a740a85696cfcf84bb2d
Jordan <jf.creative@icloud.com>
2015-02-08 11:18:21 -0700

    created gulp task to uglify (minify) main.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,35 +8,21 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-// Lint Task
-gulp.task('lint', function() {
-    return gulp.src('js/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
 
-// Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
-});
-
-// Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
+// Minify main.js in views folder JS
+gulp.task('minpizzajs', function() {
+    return gulp.src('views/js/main.js')
+        .pipe(gulp.dest('views/dist/js'))
+        .pipe(rename('main.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('views/dist/js'));
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
-});
+// gulp.task('watch', function() {
+//      gulp.watch('js/*.js', ['scripts']);
+//      gulp.watch('scss/*.scss', ['sass']);
+// });
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['minpizzajs']);
